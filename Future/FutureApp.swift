ea7862fa76d5f1ea7d3b6d1f60af67c0488bdd8e
Wu Niutian <wuniutian@gmail.com>
2022-10-22 09:27:48 +0800

    Initial Commit

---
+++ Future/FutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureApp.swift
+//  Future
+//
+//  Created by    wuniutian on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
